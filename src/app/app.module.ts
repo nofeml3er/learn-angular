368482bd5a1f9939313825c80ec5aaa29204e46c
nofeml3er <nattapong.meth@gmail.com>
2021-03-29 00:03:49 +0700

    counting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Counting } from './counting/counting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Counting
   ],
   imports: [
     BrowserModule
---
+++ src/app/counting/counting.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-counting',
+    templateUrl: './counting.component.html',
+    styleUrls: ['./counting.component.css']
+})
+
+export class Counting {
+    constructor(){}
+}
